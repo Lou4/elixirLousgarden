0a0911db0c6f55f2a4c7e3a1c582247d8debdbba
Lou4 <tagliabue.luca@gmail.com>
2015-11-27 00:37:14 +0100

    asdf

--- Field.ex
+++ Field.ex
@@ -1,6 +1,6 @@
 defmodule Field do
 	def create do
-		field  = for x <- [A, B, C, D, E, F, G, H, I, L, M, N, O, P, Q, R], y <- [A, B, C, D, E, F, G, H, I, L, M, N, O, P, Q, R], do: {x, y} 
+		field  = for x <- [A, B, C, D, E, F, G, H, I, L, M, N, O, P, Q, R], do: x
 		field
 	end
 end
